63806b91b97a5b69c37dbb359826afeb286bdced
dbvphuong <39475158+dbvphuong@users.noreply.github.com>
2018-08-10 17:02:22 +0700

    Update Count.js

--- STRING/string5/Count.js
+++ STRING/string5/Count.js
@@ -16,15 +16,11 @@ function count (str,n){
                 arr1 = [];
                 count++;
             }
-        }
-/*
-nếu số arr[i]=arr[i+1] thì thêm arr[i] vào arr1,
-nếu số arr[i] là số cuối thì count cộng 1 và bắt đầu lại vòng while
-*/
+        } /* nếu số arr[i]=arr[i+1] thì thêm arr[i] vào arr1,
+             nếu số arr[i] là số cuối thì count++ và bắt đầu lại vòng while*/
         else if (arr[i] === arr[i+1] && arr[i] === arr[j]){
             j+=1;
-        }
-// nếu 2 số liền kề bằng nhau thì j tăng 1.
+        } // nếu 2 số liền kề bằng nhau thì j tăng 1.
         else if (arr[i] === arr[i+1] && arr[i] !== arr[j]){
             arr1.push(arr.slice(i,j).length, arr[i]);
             i+=(j-i);
@@ -36,11 +32,8 @@ nếu số arr[i] là số cuối thì count cộng 1 và bắt đầu lại vò
                 arr1 = [];
                 count++;
             } 
-        }
-/*
-thêm 1 mảng có các số bằng nhau liền kề vào arr1,
-nếu mảng đó kết thúc arr thì count cộng 1 và bắt đầu lại vòng while
-*/
+        } /* thêm 1 mảng có các số bằng nhau liền kề vào arr1,
+            nếu mảng đó kết thúc arr thì count++ và bắt đầu lại vòng while*/
     }
 
     return arr.join("");
