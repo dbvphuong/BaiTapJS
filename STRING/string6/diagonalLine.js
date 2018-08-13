0b987ee834a78b689457b9d5b3b36d75ed8a52e5
dbvphuong <39475158+dbvphuong@users.noreply.github.com>
2018-08-13 14:51:44 +0700

    Update diagonalLine.js

--- STRING/string6/diagonalLine.js
+++ STRING/string6/diagonalLine.js
@@ -2,21 +2,21 @@ function zigzag (arr,arr1,i,j,n) {
 
     while (i < n && j < arr.length){
    
-        if (Math.floor(j/(n-1))%2 === 0){
+        if (Math.floor(j/(n - 1))%2 === 0){
             arr1[i][j] = arr[j];
             i++;
             j++;
-            if (i == n-1){
-                j+=n-1;
+            if (i == n - 1){
+                j += n - 1;
                 i = 0;
             }
         }// Tạo ra hàng chéo từ trên xuống dưới.
         else if(Math.floor(j/(n-1))%2 !== 0){
-            arr1[n-1-i][j] = arr[j];
+            arr1[n - 1 - i][j] = arr[j];
             i++;
             j++;
-            if ((n-1-i) == 0){
-                j+=n-1;
+            if ((n - 1 - i) == 0){
+                j += n - 1;
                 i = 0;
             }
         }// tạo ra hằng chéo từ dưới lên trên.
