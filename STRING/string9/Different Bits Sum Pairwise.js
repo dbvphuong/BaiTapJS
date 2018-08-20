59e08eb48f8681e67c827e76c2578dc201caba8a
dbvphuong <39475158+dbvphuong@users.noreply.github.com>
2018-08-20 11:24:16 +0700

    Create Different Bits Sum Pairwise.js

---
+++ STRING/string9/Different Bits Sum Pairwise.js
@@ -0,0 +1,40 @@
+function sumBits (arr) {
+    let arr1 = [];
+    let i = 0;
+    let j = 0;
+    let zero = 0;
+    let one = 0;
+    let count = 0;
+    for (let i = 0; i < arr.length ; i++){
+        let x = arr[i];
+        arr1.push(x.toString(2));
+    }// chuyển sang nhị phân
+    let max = arr1[arr1.length - 1].length;
+    for (let i = 0; i < arr1.length; i++){
+        if (arr1[i].length < max){
+            arr1[i] = new Array(max - arr1[i].length).fill(0).join('') + arr1[i]
+        }
+    }// chuyển sang cân bằng về phần tử [ '0000', '0100', '0111', '1001' ]
+
+    while (j < arr1[0].length){
+        if (i == arr1.length){
+            count = count + zero*one;
+            zero = 0;
+            one = 0;
+            i = 0;
+            j++;
+        }
+        else if (arr1[i][j] == 0){
+            zero++;
+            i++;
+        }
+        else if (arr1[i][j] == 1){
+            one++;
+            i++;
+        }
+    }// xét hàng dọc thì count = số chữ số 0 nhân với số chữ số 1, rồi cộng các count hàng dọc lại
+
+    return count*2;
+}
+console.log(sumBits([1,3,5]));
+console.log(sumBits([0,4,7,9]));
